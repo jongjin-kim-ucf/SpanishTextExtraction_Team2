fb41a8638cb8421162d822caac6ceaaa8c78718f
ibrahimalmansour1 <130081461+ibrahimalmansour1@users.noreply.github.com>
2023-04-17 14:44:27 -0400

    Update SummerizeText.py

---
+++ SummerizeText.py
@@ -0,0 +1,4 @@
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.summarizers.text_rank import TextRankSummarizer
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.utils import get_stop_words
