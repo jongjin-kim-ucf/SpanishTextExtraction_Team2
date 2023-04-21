3f910c9735f45c96ba325cf3d2d4a6397a461072
Chandra Kundu <cskundu21@gmail.com>
2023-04-21 05:25:31 -0400

    better text area detection

---
+++ FindTextAreas.py
@@ -0,0 +1,129 @@
+import cv2
+import numpy as np
+from imutils.object_detection import non_max_suppression
+
+def detecttextarea(img_path, out_path):    
+    # source: https://stackoverflow.com/posts/72101499/revisions
+    
+    image = cv2.imread(img_path)
+    orig = image.copy()
+    (H, W) = image.shape[:2]
+    (newW, newH) = (1280, 736)
+    rW = W / float(newW)
+    rH = H / float(newH)
+    image = cv2.resize(image, (newW, newH))
+    (H, W) = image.shape[:2]
+    gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # define the two output layer names for the EAST detector model that
+    # we are interested -- the first is the output probabilities and the
+    # second can be used to derive the bounding box coordinates of text
+    layerNames = [
+        "feature_fusion/Conv_7/Sigmoid",
+        "feature_fusion/concat_3"]
+
+    # load the pre-trained EAST text detector
+    print("[INFO] loading EAST text detector...")
+    net = cv2.dnn.readNet('frozen_east_text_detection.pb')
+
+    # construct a blob from the image and then perform a forward pass of
+    # the model to obtain the two output layer sets
+    blob = cv2.dnn.blobFromImage(image, 1.0, (W, H),(123.68, 116.78, 103.94), swapRB=True, crop=False)
+    net.setInput(blob)
+    (scores, geometry) = net.forward(layerNames)
+
+    # grab the number of rows and columns from the scores volume, then
+    # initialize our set of bounding box rectangles and corresponding
+    # confidence scores
+    (numRows, numCols) = scores.shape[2:4]
+    rects = []
+    confidences = []
+
+    # loop over the number of rows
+    for y in range(0, numRows):
+        # extract the scores (probabilities), followed by the geometrical
+        # data used to derive potential bounding box coordinates that
+        # surround text
+        scoresData = scores[0, 0, y]
+        xData0 = geometry[0, 0, y]
+        xData1 = geometry[0, 1, y]
+        xData2 = geometry[0, 2, y]
+        xData3 = geometry[0, 3, y]
+        anglesData = geometry[0, 4, y]
+        
+        for x in range(0, numCols):
+            # ignore probability values below 0.75
+            if scoresData[x] < 0.99:
+                continue
+            
+            # compute the offset factor as our resulting feature maps will
+            # be 4x smaller than the input image
+            (offsetX, offsetY) = (x * 4.0, y * 4.0)
+            
+            # extract the rotation angle for the prediction and then
+            # compute the sin and cosine
+            angle = anglesData[x]
+            cos = np.cos(angle)
+            sin = np.sin(angle)
+            
+            # use the geometry volume to derive the width and height of
+            # the bounding box
+            h = xData0[x] + xData2[x]
+            w = xData1[x] + xData3[x]
+            
+            # compute both the starting and ending (x, y)-coordinates for
+            # the text prediction bounding box
+            endX = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
+            endY = int(offsetY - (sin * xData1[x]) + (cos * xData2[x]))
+            startX = int(endX - w)
+            startY = int(endY - h)
+            
+            # add the bounding box coordinates and probability score to
+            # our respective lists
+            rects.append((startX, startY, endX, endY))
+            confidences.append(scoresData[x])
+
+    # apply non-maxima suppression to suppress weak, overlapping bounding
+    # boxes
+    boxes = non_max_suppression(np.array(rects), probs=confidences)
+
+    results = []
+    # loop over the bounding boxes
+    for (startX, startY, endX, endY) in boxes:
+        # scale the bounding box coordinates based on the respective
+        # ratios
+        startX = int(startX * rW)
+        startY = int(startY * rH)
+        endX = int(endX * rW)
+        endY = int(endY * rH)
+
+        cv2.rectangle(orig, (startX, startY), (endX, endY), (0, 255, 0), 2)
+
+
+        ## ----------- OCR CODE -------------- ##
+        w = endX - startX
+        h = endY - startY
+
+        crop_img = gray_img[endY:endY+h, startX:startX+w]
+
+        
+        # Pass the cropped image to Tesseract OCR and get the output text
+        # ocr_output = pytesseract.image_to_string(crop_img, lang='spa')
+
+        # if ocr_output.strip():
+        #     results.append((x, y, w, h, ocr_output.strip()))
+
+        # draw the bounding box on the image
+        
+
+    cv2.imwrite(out_path, orig)
+    
+    return orig, results
+
+
+if __name__ == '__main__':
+    for i in range(1, 5):
+        img_path = f"training/training{i}.png"
+        out_path = f"boxed_images/rectanglebox{i}.jpg"
+        rects, texts = detecttextarea(img_path, out_path)   
+                
